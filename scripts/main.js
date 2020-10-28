ebb562f5935700d4c6ee848e0ecfde40ca947309
Dillon Draper <dillonbdraper@gmail.com>
2020-10-28 14:02:06 -0500

    Needed a new branch/merge to fix funcitonality issues for other devs

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,10 @@
-import { stateChanged } from './states/StateSelect.js'
+import { StateSelect } from './states/StateSelect.js'
 import { getAttractions, useAttractions } from "./attractions/AttractionProvider.js";
 import { getParks, useParks } from "./parks/ParkProvider.js";
 import { getEateries, useEateries } from "./eateries/EateryProvider.js"
 import { getWeather } from "./weather/WeatherProvider.js";
-import { StateSelect } from "./states/StateSelect.js";
 import { ParksSelect } from "./parks/ParkSelect.js";
 
-getAttractions().then(
-    () => console.log(useAttractions())
-)
 
 getEateries().then(
     () => console.log("This is a slice of the eateries array: ", useEateries())
@@ -16,8 +12,8 @@ getEateries().then(
 
 
 console.log("hello")
-
+StateSelect()
 getWeather()
 StateSelect()
 ParksSelect()
-stateChanged()
+StateSelect()
