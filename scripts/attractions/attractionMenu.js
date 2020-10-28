c736e3ce04b218a2b9e4818b300e34dc02577e18
Dillon Draper <dillonbdraper@gmail.com>
2020-10-28 10:38:14 -0500

    Need to use new master

--- scripts/attractions/attractionMenu.js
+++ scripts/attractions/attractionMenu.js
@@ -1,17 +1,20 @@
-const { useAttractions } = require("./AttractionProvider")
+import { getAttractions, useAttractions } from "./AttractionProvider.js"
 
 const state = document.getElementById("stateSelect")
 const attractionsSelect = document.getElementById("bizzarariesSelect")
 const eventHub = document.querySelector(".container")
 
 const populateAttractionMenu = (stateCode) => {
-    const bizzararies = useAttractions().filter(stateObj => {
+    const bizzararies = getAttractions().then(
+        useAttractions().filter(stateObj => {
         return stateObj.state = stateCode
-    })
+    }))
 
     const htmlToAssign = bizzaries.forEach(attraction => {
         return `
-            
+            <option value="${attraction.id}">${attraction.name}> 
         `
     });
+
+    attractionsSelect.innerHTML = htmlToAssign
 }
