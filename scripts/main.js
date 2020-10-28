854e6f439b5d8f2e89181a28343265a133b2008d
Travis Stevenson <thetechtravis@outlook.com>
2020-10-28 10:20:30 -0500

    Created getEateries and useEateries functions. getEateries() is responsible for fetching the /eateries endpoint, converting it to .JSON format, and assigning the value to the eateries array. useEateries() is responsible for returning a slice of the eateries array, for data manipulation purposes. Imported getEateries() and useEateries() into main.js file. Called getEateries() in main.js to test functionality.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 import { getParks, useParks } from "./parks/ParkProvider.js";
+import { getEateries, useEateries } from "./eateries/EateryProvider.js"
 
 getParks().then(
     () => console.log(useParks())
 )
+
+getEateries().then(
+    () => console.log("This is a slice of the eateries array: ", useEateries())
+)
+
 
 console.log("hello")
